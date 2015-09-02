15011edd1b1a945e4955155f32a2361f1c6e3d58
Scott Sanford <ssanford@mediafly.com>
2015-09-02 16:51:39 -0500

    add google maps

---
+++ www/js/controllers.js
@@ -0,0 +1,31 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('GoogleMapCtrl', function($scope, Chats) {
+    $scope.map = { 
+      center: { 
+          latitude: 41.881832, 
+          longitude: -87.623177
+          }, 
+      zoom: 13
+    };
+
+    $scope.marker = {
+      id: 0,
+      coords: {
+          latitude: 41.881832, 
+          longitude: -87.623177 
+      }
+    }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
