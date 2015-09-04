c5f6f86606af20c97cdc50d5603fd147ef21d2de
Scott Sanford <ssanford@mediafly.com>
2015-09-04 16:29:18 -0500

    add meeting list to search view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,47 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
-
+    $scope.places = [
+                {"type": "Coffee","checked": false},
+                {"type": "Food","checked": false},
+                {"type": "Drinks", "checked": false}
+            ]
 })
 
-.controller('GoogleMapCtrl', function($scope, Chats, $cordovaGeolocation) {
-   $scope.map = { 
-      center: { 
-          latitude: 41.881832, 
-          longitude: -87.623177
-          }, 
-      zoom: 13
-    };
-
-    $scope.marker = {
-      id: 0,
-      coords: {
-          latitude: 41.881832, 
-          longitude: -87.623177 
+.controller('GoogleMapCtrl', function($scope, Chats, uiGmapGoogleMapApi) {
+       $scope.myLocation = {
+        lng : '',
+        lat: ''
       }
-    }
+       
+      $scope.drawMap = function(position) {
+     
+        //$scope.$apply is needed to trigger the digest cycle when the geolocation arrives and to update all the watchers
+        $scope.$apply(function() {
+          $scope.myLocation.lng = position.coords.longitude;
+          $scope.myLocation.lat = position.coords.latitude;
+     
+          $scope.map = {
+            center: {
+              latitude: $scope.myLocation.lat,
+              longitude: $scope.myLocation.lng
+            },
+            zoom: 14,
+            pan: 1
+          };
+     
+          $scope.marker = {
+            id: 0,
+            coords: {
+              latitude: $scope.myLocation.lat,
+              longitude: $scope.myLocation.lng
+            }
+          }; 
+           
+        });
+      }
+     
+      navigator.geolocation.getCurrentPosition($scope.drawMap);  
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
