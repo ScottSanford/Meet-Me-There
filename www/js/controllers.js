899e628a8be60604901c6889f8f3440ae181f31c
Scott Sanford <ssanford@mediafly.com>
2015-10-07 13:13:36 -0500

    Add Google Directions

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,27 +18,62 @@ angular.module('starter.controllers', [])
 
       $scope.loading = $ionicLoading.show();
 
+
       // get position of user and then set the center of the map to that position
       $cordovaGeolocation
         .getCurrentPosition()
         .then(function (position) {
+
+          // get coordinates of user
           var latitude  = position.coords.latitude
           var longitude = position.coords.longitude
-          $scope.map = {
-            center: {
-              latitude: latitude, 
-              longitude: longitude
-            }, 
-            zoom: 14 
-          };
-          $scope.marker = {
-            id: 0,
-            coords: {
-              latitude: latitude, 
-              longitude: longitude
+
+          function initGoogleMap() {
+            var directionsService = new google.maps.DirectionsService;
+            var directionsDisplay = new google.maps.DirectionsRenderer;
+            var googleMapOptions = {
+                center: {
+                  lat: latitude, 
+                  lng: longitude
+                }, 
+                zoom:15
+            }
+
+            var map = new google.maps.Map(document.getElementById('map'), googleMapOptions);
+
+            // create Marker on the map to show location
+            var marker = new google.maps.Marker({
+              position: googleMapOptions.center,
+              map: map,
+            });
+
+            $scope.getDirections = function() {
+              calculateAndDisplayRoute(directionsService, directionsDisplay, map);
             }
           }
+
+          initGoogleMap();
+          // hide Ionic Loading Icon
           $ionicLoading.hide();
+
+          // // get directions using google maps api
+         function calculateAndDisplayRoute(directionsService, directionsDisplay, map) {
+            directionsService.route({
+              origin: 'Chicago, IL',
+              destination: 'St. Louis, MO',
+              travelMode: google.maps.TravelMode.DRIVING
+            }, function(response, status) {
+              if (status === google.maps.DirectionsStatus.OK) {
+                console.log("Success=", response);
+                console.log('map', map);
+                directionsDisplay.setMap(map);
+                directionsDisplay.setDirections(response);
+              } else {
+                console.log('Directions request failed due to ' + status);
+              }
+            });
+          }
+
         }, function(err) {
           console.log('there was an error', err);
         });
